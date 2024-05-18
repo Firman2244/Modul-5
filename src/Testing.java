c49f19b57b5e4db15b7d5ec47b4094c00525bb3f
Firman2244 <firmanaregas10@gmail.com>
2024-05-18 13:08:11 +0700

    first commit

---
+++ src/Testing.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Testing {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        ArrayList<String>
+    }
+}
